163fa10d69438916e3336b722cd8395b1f971e20
xiaojl <xiaojinlong@me.com>
2021-06-17 09:20:15 +0800

    测试提交

--- rain-controller/src/main/java/org/xiao/rain/controller/servlet/TestServlet.java
+++ rain-controller/src/main/java/org/xiao/rain/controller/servlet/TestServlet.java
@@ -19,10 +19,7 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("test servlet");
-        
         TestService.testService();
-        
     }
 
 }
