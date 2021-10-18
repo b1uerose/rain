a6e5dfbe320d9c56d24e1fa3fd4e0908a348f9aa
xiaojl <xiaojinlong@me.com>
2021-10-18 22:18:29 +0800

    gittest

---
+++ rain-controller/src/test/java/org/xiao/rain/test/git/GitTest02.java
@@ -0,0 +1,12 @@
+package org.xiao.rain.test.git;
+
+/**
+ * Author: xiaojl
+ * Date: 2021/10/18 22:17
+ */
+public class GitTest02 {
+
+    public static void main(String[] args) {
+        System.out.println("test01");
+    }
+}
