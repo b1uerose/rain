d89d98c96f5bb183da2823f05b399e7fe4035d71
xiaojl <xiaojinlong@me.com>
2021-10-18 20:51:19 +0800

    测试git

---
+++ rain-controller/src/test/java/org/xiao/rain/test/git/GitTest.java
@@ -0,0 +1,13 @@
+package org.xiao.rain.test.git;
+
+/**
+ * Author: xiaojl
+ * Date: 2021/10/18 20:50
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+    }
+}
