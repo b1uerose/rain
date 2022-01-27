3bb1252f95cba25e7b84a0a1a5f8516bac1f54a6
xiaojl <xiaojinlong@me.com>
2022-01-27 16:31:37 +0800

    提交代码

--- rain-service/src/main/java/org/xiao/rain/exception/BusinessException.java
+++ rain-service/src/main/java/org/xiao/rain/exception/BusinessException.java
@@ -15,6 +15,10 @@ public class BusinessException extends Exception {
     public BusinessException(String message) {
         super(message);
     }
+    
+    public BusinessException(Exception e) {
+        super(e);
+    }
 
     public BusinessException(String message, Throwable cause) {
         super(message, cause);
