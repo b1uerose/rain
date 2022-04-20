7b70c6966472cd3298df84cd9e1f16281d506f4d
xiaojl <xiaojinlong@me.com>
2022-04-20 22:57:16 +0800

    算法等

--- rain-controller/src/test/java/org/xiao/rain/test/spring/ioc/beans/SpringIOCChild01.java
+++ rain-controller/src/test/java/org/xiao/rain/test/spring/ioc/beans/SpringIOCChild01.java
@@ -5,4 +5,8 @@ package org.xiao.rain.test.spring.ioc.beans;
  * Date:2022-04-18 14:35
  */
 public class SpringIOCChild01 {
+    
+    public void sayHello() {
+        System.out.println("hello, i am child01");
+    }
 }
