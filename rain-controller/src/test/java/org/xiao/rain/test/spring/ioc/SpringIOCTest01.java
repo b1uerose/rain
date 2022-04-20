19137152c23d11fef18e08e214ce33228971ed69
xiaojl <xiaojinlong@me.com>
2022-04-20 23:11:37 +0800

    spring

--- rain-controller/src/test/java/org/xiao/rain/test/spring/ioc/SpringIOCTest01.java
+++ rain-controller/src/test/java/org/xiao/rain/test/spring/ioc/SpringIOCTest01.java
@@ -1,8 +1,10 @@
 package org.xiao.rain.test.spring.ioc;
 
 import org.junit.Test;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.xiao.rain.test.spring.ioc.beans.SpringIOCService;
 
 /**
@@ -14,10 +16,24 @@ public class SpringIOCTest01 {
     
     @Test
     public void springTest01() throws Exception {
-        
         String configName = "newBeans.xml";
         ApplicationContext context = new ClassPathXmlApplicationContext(configName);
-        SpringIOCService testService = (SpringIOCService) context.getBean("springioc");
+        SpringIOCService testService = context.getBean("springiocservice", SpringIOCService.class);
         testService.userXMLConfig();
+        testService.getChild01().sayHello();
     }
+    
+    @Test
+    public void springTest02() throws Exception {
+
+        GenericApplicationContext context = new GenericApplicationContext();
+        new XmlBeanDefinitionReader(context).loadBeanDefinitions("newBeans.xml");
+        context.refresh();
+
+        SpringIOCService springiocservice = context.getBean("springiocservice", SpringIOCService.class);
+        
+        springiocservice.userXMLConfig();
+
+    }
+    
 }
